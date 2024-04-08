c09dbfd6f45b30b6722375145c2de41ba6cfafb5
JiWonOck <dhrwl541@naver.com>
2024-04-07 20:26:36 -0400

    Add byte-order-test

--- main.cpp
+++ main.cpp
@@ -20,17 +20,25 @@ void write_4660() {
 	dump(&port, sizeof(port));
 }
 
+uint16_t my_ntohs(uint16_t a){
+	return ((a &0xFF00)>> 8) | ((a&0x00FF) << 8);
+}
+
 void  write_0x1234() {
 	uint8_t network_buffer[] = { 0x12, 0x34 };
 	uint16_t* p = reinterpret_cast<uint16_t*>(network_buffer);
-	uint16_t n = *p; // TODO
+	uint16_t n = my_ntohs(*p); // TODO
 	printf("16 bit number=0x%x\n", n);
+}
+
+uint32_t my_ntohl(uint32_t a){
+	return (a>>24 & 0xFF) | (a>>8 & 0xFF00) | (a<<8 & 0xFF0000) | (a<<24 & 0xFF000000);
 }
 
 void  write_0x12345678() {
 	uint8_t network_buffer[] = { 0x12, 0x34, 0x56, 0x78 };
 	uint32_t* p = reinterpret_cast<uint32_t*>(network_buffer);
-	uint32_t n = *p; // TODO
+	uint32_t n = my_ntohl(*p); // TODO
 	printf("32 bit number=0x%x\n", n);
 }
 
